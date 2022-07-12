12e79c54c8d6c2cd71c2b453c4977b54ec802114
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-07-12 17:53:12 +0100

    Adding flutter application

---
+++ blog_app/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'blog.dart';
+
+void main() {
+  runApp(const BlogApp());
+}
+
+const blackColor = Colors.black;
+const whiteColor = Colors.white;
+
+class BlogApp extends StatelessWidget {
+  const BlogApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Blog",
+      home: const BlogPage(title: "Blog"),
+    );
+  }
+}
+
+class BlogPage extends StatefulWidget {
+  const BlogPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+
+}
+
+class _BlogPageState extends State<BlogPage> {
+  late Future<List<Post>> futurePost;
+  // late Future<Post> futureDetail;
+
+  @override
+  void initState(){
+    super.initState();
+    futurePost = fetchPost();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: whiteColor,
+        foregroundColor: blackColor,
+      ),
+      body: SafeArea(
+        child: FutureBuilder<List<Post>>(
+          future: futurePost,
+          builder: (context, snapshot) {
+            if(snapshot.hasData){
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index){
+                  return GestureDetector(
+                    onTap: (){
+                      int b;
+                    },
+                    child: BlogCard(snapshot.data![index]),
+                  );
+                },
+              );
+            } else{
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
+  }
+  Widget BlogCard(Post post){
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: <Widget>[
+          Image(image: NetworkImage(post.image)),
+          const SizedBox(
+            height: 14.0,
+          ),
+          Text(
+            post.title,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino'
+            ),
+          ),
+        ],
+        
+        ),
+      ),
+    );
+  }
+}
